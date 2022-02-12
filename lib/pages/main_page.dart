e7bad04d7c970a80584a9e826463227ae221bb00
Nailul Firdaus <nail.nafir@gmail.com>
2022-02-12 17:20:13 +0700

    add main page

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,19 @@
+part of 'pages.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
