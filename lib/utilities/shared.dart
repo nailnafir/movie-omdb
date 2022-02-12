c73b5aa1d3f041b8d27d0a9f63e854e4e1c2306c
Nailul Firdaus <nail.nafir@gmail.com>
2022-02-13 00:31:34 +0700

    add utilities shared

---
+++ lib/utilities/shared.dart
@@ -0,0 +1,11 @@
+part of 'utilities.dart';
+
+class ContactUrl {
+  static String telegramURL = "https://t.me/nailnafir";
+  static String emailURL = "mailto:nailnafir@outlook.com";
+}
+
+class ApiUrl {
+  static String apiKey = 'a19c6462';
+  static String baseUrl = 'omdbapi.com';
+}
--- lib/utilities/utilities.dart
+++ lib/utilities/utilities.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 part 'config.dart';
-
 part 'theme.dart';
+part 'shared.dart';
