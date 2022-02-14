0aac4637fb2c92eff2359a0d8fe57808437afc15
Nailul Firdaus <nail.nafir@gmail.com>
2022-02-14 19:03:41 +0700

    delete state get

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_movie/pages/pages.dart';
-import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      home: MainPage(),
+    return const MaterialApp(
+      home: MainPage(initialPage: 0),
     );
   }
 }
